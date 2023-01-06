d612e92129a16283800097faeaf5a17b63711c69
Minori-Takeuchi <shato.min0716@gmeail.com>
2023-01-06 13:15:47 +0900

    first commit

---
+++ todoapp/resources/views/index.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+  <title>COACHTECH</title>
+</head>
+<body>
+  <div class="container">
+    <div class="card">
+      <p class="ttl">Todo List</p>
+      <div class="todo">
+        @if (count($errors) > 0)
+          <ul>
+            <li>タスクを入力してください。</li>
+          </ul>
+        @endif
+        <form action="/create" method="post" class="frex">
+          @csrf
+          <input type="text" class="input-create" name="name">
+          <button class="btn-create">追加</button>
+        </form>
+        <table>
+          <tr>
+            <th>作成日</th>
+            <th>タスク名</th>
+            <th>更新</th>
+            <th>削除</th>
+          </tr>
+          @foreach($todos as $todo)
+          <tr>
+            <td>{{$todo->created_at}}</td>
+            <form action="/update" method="post">
+            @csrf
+              <td><input type="text" class="input-update" name="name" value="{{$todo->name}}"></td>
+              <input type="hidden" name="id" value="{{$todo->id}}">
+              <td><button class="btn-update">更新</button></td>
+            </form>
+            <form action="/delete" method="post">
+            @csrf
+              <input type="hidden" name="id" value="{{$todo->id}}">
+              <td><button class="btn-delete">削除</button></td>
+            </form>
+            </tr>
+            @endforeach
+          </table>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
